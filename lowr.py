b5874fbdcf86d0e8fa1a45af24391abc0f8513e3
drytuna <drytuna@gmail.com>
2014-06-24 13:47:35 -0700

    populate search results into a Priority Queue

--- lowr.py
+++ lowr.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_page():
-    return render_template('base.html')
+    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
