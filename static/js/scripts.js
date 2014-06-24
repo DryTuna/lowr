2118c9ffaae175b66ef533c41bcc9395a607a186
Jonathan Siebert <jon.siebert@gmail.com>
2014-06-24 10:36:14 -0700

    Split static html into flask templates, unfinished

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -9,3 +9,21 @@ function toggleRowSelect(row_id) {
         selectedProducts.push(row_id);
     }
 }
+
+
+$('document').ready(function(){
+    $('#search_form').on('submit', function(event) {
+          event.preventDefault();
+          var search_form =  $(event.target);
+          $.ajax(search_form.action),{
+              type: 'GET',
+              data: JSON.stringify(search_form.serializeArray()),
+              context: search_form,
+              success: function(result){
+                  var parent = $(this).parent();
+                  parent.empty().html(result);
+
+              }
+          });
+    });
+})
