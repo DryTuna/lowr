028aa2414e5ae926fdfeee9af4843cc7ec4fafd9
Jonathan Siebert <jon.siebert@gmail.com>
2014-06-24 09:25:31 -0700

    Started preparing form to submit items to track

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,11 @@
-function toggleRowSelect(row) {
-    if($(row).hasClass("warning"))
-        $(row).removeClass("warning");
-    else
-        $(row).addClass("warning");
+// maintain a list of the products selected, so we can submit them
+selectedProducts = []
+function toggleRowSelect(row_id) {
+    if($(row_id).hasClass("warning")) {
+        $(row_id).removeClass("warning");
+        selectedProducts.splice(selectedProducts.indexOf(row_id), 1);
+    } else {
+        $(row_id).addClass("warning");
+        selectedProducts.push(row_id);
+    }
 }
