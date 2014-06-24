e559e4a2629e5915a490d18098680848884863aa
drytuna <drytuna@gmail.com>
2014-06-24 11:40:04 -0700

    Added / route

--- lowr.py
+++ lowr.py
@@ -3,12 +3,18 @@ from flask import render_template
 from amazon_book_scraper import *
 
 
+cat = {
+    'Electronics': 'n:283155',
+    'Books': 'n:283155'
+}
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def main():
-    return render_template()
+    return render_template("main.html")
 
 
 if __name__ == '__main__':
