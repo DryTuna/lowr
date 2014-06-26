ba96fec8b26d9290591a285396e8b4f07179aaee
eyueld-codefellow <eyueld-codfefellow@gmail.com>
2014-06-26 10:39:01 -0700

    Made changes to lowr to fix error notification

--- lowr.py
+++ lowr.py
@@ -133,7 +133,7 @@ def signup():
                 session['username'] = username
                 return redirect(url_for('home_page'))
         else:
-            error = {error: "Passwords do not match."}
+            error = {'error': "Passwords do not match."}
 
     return render_template('signup.html', error=error)
 
@@ -218,53 +218,6 @@ app.config['ADMIN_PASSWORD'] = os.environ.get(
 app.config['SECRET_KEY'] = os.environ.get(
     'FLASK_SECRET_KEY', 'sooperseekritvaluenooneshouldknow'
 )
-
-
-
-DB_SCHEMA = """
-DROP TABLE IF EXISTS accounts;
-CREATE TABLE accounts (
-    id serial PRIMARY KEY,
-    username VARCHAR (127) NOT NULL UNIQUE,
-    email VARCHAR (127) NOT NULL,
-    password VARCHAR (127) NOT NULL,
-    created TIMESTAMP NOT NULL
-)
-"""
-
-
-
-DB_ENTRIES_LIST = """
-SELECT id, username , email, password FROM accounts ORDER BY created DESC
-"""
-
-DB_ENTRY_LIST = """
-SELECT id, username, email, password created FROM accounts WHERE accounts.id = %s
-"""
-
-DB_DELETE_ENTRY_LIST = """
-DELETE FROM accounts WHERE accounts.id = %s
-"""
-
-def connect_db():
-    """Return a connection to the configured database"""
-    return psycopg2.connect(app.config['DATABASE'])
-
-def init_db():
-    """Initialize the database using DB_SCHEMA
-
-    WARNING: executing this function will drop existing tables.
-    """
-    with closing(connect_db()) as db:
-        db.cursor().execute(DB_SCHEMA)
-        db.commit()
-
-
-def get_database_connection():
-    db = getattr(g, 'db', None)
-    if db is None:
-        g.db = db = connect_db()
-    return db
 
 
 @app.teardown_request
