093f4e44d09d2a8e835e2616ec1cee9254603a6a
Jonathan Siebert <jon.siebert@gmail.com>
2014-06-26 12:59:12 -0700

    Fixed javascript to collect objects to represent products the user is tracking

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,34 +2,52 @@
 
 
 
-selectedProductURLs = []
+selectedProducts = []
 function toggleRowSelect(product_url) {
     product_listing = $(document.getElementById(product_url));
     if(product_listing.hasClass("warning")) {
         product_listing.removeClass("warning");
-        selectedProductURLs.splice(selectedProductURLs.indexOf(product_url), 1);
+        for(i = 0; i < selectedProducts.length; i++) {
+            if(selectedProducts[i].url == product_url) {
+                selectedProducts.splice(selectedProducts.indexOf(product_url), 1);
+                break;
+            }
+        }
     } else {
         product_listing.addClass("warning");
-        selectedProductURLs.push(product_url);
+        // whichever is less, the prime price or the price
+        tds = document.getElementById(product_url).getElementsByTagName('td');
+        price_col = tds[tds.length - 1];
+        prime_price_col = tds[tds.length - 2];
+        var prime_price = parseFloat(prime_price_col.innerHTML.replace("$", "").trim());
+        var price = parseFloat(price_col.innerHTML.replace("$", "").trim());
+        if(isNaN(price) || Math.round(price) == 0) {
+            price = prime_price;
+        } else if(!isNaN(prime_price) && Math.round(prime_price) != 0) {
+          price = Math.min(price, prime_price);
+        }
+        selectedProducts.push({"url": product_url,
+                               "desired_price": parseFloat(document.getElementById("search_min_price").value),
+                               "price": price});
     }
 }
 
 function submitItems() {
-          $.ajax(("/submititems"), {
-                type: 'POST',
-                data: JSON.stringify(selectedProductURLs),
-                contentType: 'application/json',
-                context: document.body,
-                success: function(result) {
-                    location.href = result;
-                }
-            });
+  $.ajax(("/submititems"), {
+        type: 'POST',
+        data: JSON.stringify(selectedProducts),
+        contentType: 'application/json',
+        context: document.body,
+        success: function(result) {
+            location.href = result;
         }
+    });
+}
 
 $('document').ready(function() {
 
     $('#search_form').on('submit', function(event) {
-                  selectedProductURLs = []
+                  selectedProducts = []
                   event.preventDefault();
                   var search_form =  $(event.target);
                   var formJsonData = JSON.stringify($(this).serializeArray());
