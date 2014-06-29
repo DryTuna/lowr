8d035d6a770e2db263222dc42d621b8477c288fc
Jonathan Siebert <jon.siebert@gmail.com>
2014-06-29 00:54:11 -0700

    Made it so an exception in the scraping code is emailed to admin, and doesn't break the site

--- amazon_scraper.py
+++ amazon_scraper.py
@@ -152,27 +152,31 @@ def page_scrape(keywords, category, page, tQ):
 
 def search_results(keywords, category, price, price_range):
     from multiprocessing import Process, Queue
-    page = 1
-    category = set_globals(category, price, price_range)
+    from crawler import error_email
     p_queue = P_Queue()
-    while p_queue._size < 5:
-        temp = p_queue._size
-        tQ = Queue()
-        proc_1 = Process(target=page_scrape, args=(keywords, category, page, tQ))
-        proc_2 = Process(target=page_scrape, args=(keywords, category, page+1, tQ))
-        proc_3 = Process(target=page_scrape, args=(keywords, category, page+2, tQ))
-        proc_1.start()
-        proc_2.start()
-        proc_3.start()
-        proc_1.join()
-        proc_2.join()
-        proc_3.join()
-        while not tQ.empty():
-            x = tQ.get()
-            p_queue.insert(x[0], x[1])
-        page += 3
-        if p_queue._size == temp:
-            break
+    try:
+        page = 1
+        category = set_globals(category, price, price_range)
+        while p_queue._size < 5:
+            temp = p_queue._size
+            tQ = Queue()
+            proc_1 = Process(target=page_scrape, args=(keywords, category, page, tQ))
+            proc_2 = Process(target=page_scrape, args=(keywords, category, page+1, tQ))
+            proc_3 = Process(target=page_scrape, args=(keywords, category, page+2, tQ))
+            proc_1.start()
+            proc_2.start()
+            proc_3.start()
+            proc_1.join()
+            proc_2.join()
+            proc_3.join()
+            while not tQ.empty():
+                x = tQ.get()
+                p_queue.insert(x[0], x[1])
+            page += 3
+            if p_queue._size == temp:
+                break
+    except Exception as e:
+        error_email(e)
     return p_queue
 
 
